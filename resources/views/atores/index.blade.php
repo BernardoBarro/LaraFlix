f7978552f61a16b6edd5de8883da6c40d79187cb
BernardoBarro <bernardo.barro@gmail.com>
2022-04-16 01:51:56 -0300

    Begin project

---
+++ resources/views/atores/index.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('content')
+    <h1>Atores</h1>
+    <table class="table table-stripe table-bordered table-hover">
+        <thead>
+            <th>Nome</th>
+            <th>Data de Nascimento</th>
+            <th>Ações</th>
+        </thead>
+
+        <tbody>
+            @foreach($atores as $ator)
+                <tr>
+                    <td>{{ $ator->nome }}</td>
+                    <td>{{ $ator->dt_nascimento }}</td>
+                    <td>
+                        <a href="{{ route('atores.edit', ['id'=>$ator->id]) }}" class="btn-sm btn-success">Editar</a>
+                        <a href="{{ route('atores.destroy', ['id'=>$ator->id]) }}" class="btn-sm btn-danger">Remover</a>
+                    </td>
+                    <br>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="{{ route('atores.create', []) }}" class="btn btn-info">Adicionar</a>
+@stop
